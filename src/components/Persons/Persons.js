9771f768f23fa0faf810e9cfd07087622c2b4d7e
pooja-kapuriya <pooja.kapuriya@neosofttech.com>
2018-09-25 19:00:54 +0530

    87-Component Updating Lifecycle in Action

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,28 +2,46 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
-    constructor(props){
-        super(props);
-        console.log("[Persons.js] Inside constructor", props);
-      }
-    
-      componentWillMount() {
-        console.log("[Persons.js] Inside componentWillMount()");    
-      }
-    
-      componentDidMount() {
-        console.log("[Persons.js] Inside componentDidMount()");    
-      }
-    render() {
-        console.log("[Persons.js] Inside render()"); 
-        return this.props.persons.map( (person, index) => {
-            return <Person 
-                click={() => this.props.clicked( index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.props.changed( event, person.id)} />
-        } );
-    }
+	constructor(props){
+		super(props);
+		console.log("[Persons.js] Inside constructor", props);
+	}
+
+	componentWillMount() {
+		console.log("[Persons.js] Inside componentWillMount()");    
+	}
+
+	componentDidMount() {
+		console.log("[Persons.js] Inside componentDidMount()");    
+	}
+
+	componentWillReceiveProps(nextProps) {
+		console.log("[UPDATE Persons.js] Inside componentWillReceiveProps", nextProps);    
+	}
+
+	shouldComponentUpdate(nextProps, nextState){
+		console.log("[UPDATE Persons.js] Inside shouldComponentUpdate nextProps: ", nextProps, "nextState : ", nextState);    
+		return nextProps.persons !== this.props.persons;
+	}
+
+	componentWillUpdate(nextProps, nextState){
+		console.log("[UPDATE Persons.js] Inside componentWillUpdate nextProps: ", nextProps, "nextState : ", nextState);    
+	}
+
+	componentDidUpdate() {
+		console.log("[UPDATE Persons.js] Inside componentDidUpdate()"); 
+	}
+
+	render() {
+		console.log("[Persons.js] Inside render()"); 
+		return this.props.persons.map( (person, index) => {
+			return <Person 
+				click={() => this.props.clicked( index)}
+				name={person.name}
+				age={person.age}
+				key={person.id}
+				changed={(event) => this.props.changed( event, person.id)} />
+		} );
+	}
 }
 export default Persons;
